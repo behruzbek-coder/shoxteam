d5bd095ba2f29a861f3de358a095329b4d6e5ac1
1Shox <shaxriyorqobiljonov18@gmail.com>
2024-08-13 06:58:00 +0100

    asdsaasd

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <NavComponent />
+  </div>
+</template>
+
+<script setup>
+import NavComponent from './components/NavComponent.vue';
+</script>
+
+<style lang="scss" scoped>
+
+</style>
