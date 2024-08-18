925284bdf8b07d10b21b0f684b3f6afb7c483075
RevinhoDev <revinhodev@gmail.com>
2024-08-18 15:07:36 +0500

    Revinho

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <template>
   <div>
-    <ProductComponent/>
+    <SignUpComponent></SignUpComponent>
   </div>
 </template>
 
 <script setup>
 import ProductComponent from "@/components/ProductComponent.vue";
+import SignUpComponent from "@/components/SignUpComponent.vue";
 
 </script>
