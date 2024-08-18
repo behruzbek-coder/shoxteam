4a752eb30903623e5cf6f91f67c5b468957bcc61
RevinhoDev <revinhodev@gmail.com>
2024-08-18 12:31:44 +0500

    Export

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,54 @@
 <template>
   <div>
-    <NavComponent />
+    <h1>Processed Data from API:</h1>
+    <ul v-if="processedData.length">
+      <li v-for="item in processedData" :key="item.id">
+        {{ item.displayName }}
+      </li>
+    </ul>
+    <p v-else>No data available</p>
+    <p v-if="loading">Loading...</p>
+    <p v-if="error">{{ error.message }}</p>
   </div>
 </template>
 
 <script setup>
-import NavComponent from './components/NavComponent.vue';
+import { ref, onMounted } from "vue";
+
+let data = [];
+const processedData = ref([]);
+const loading = ref(true);
+const error = ref(null);
+
+const processData = () => {
+  console.log(data)
+  data.value.forEach(element => {
+    console.log(element.name)
+  });
+};
+
+const fetchData = async () => {
+  loading.value = true;
+  try {
+    const response = await fetch(
+      "https://full-api.onrender.com/api/product/get"
+    );
+    if (!response.ok) {
+      throw new Error("Error");
+    }
+    const result = await response.json();
+    console.log(result);
+    data.value = result.data;
+    processData();
+  } catch (err) {
+    error.value = err;
+    console.error("Error:", err);
+  } finally {
+    loading.value = false;
+  }
+};
+
+onMounted(() => {
+  fetchData();
+});
 </script>
-
-<style lang="scss" scoped>
-
-</style>
---
+++ src/components/productC.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="my-product-component">
+    <div class="top-of-product">
+      <p>Артикул: {{ articleID }}</p>
+      <p>{{ productType }}</p>
+      <img src="@/assets/images/Vector.svg" />
+    </div>
+    <img :src="imageURL" />
+    
+  </div>
+</template>
+    
+    <script>
+export default {
+  name: "ProductComponent",
+  props: {
+    productType: {
+      type: String,
+      default: "none",
+    },
+    articleID: {
+      type: String,
+      default: "none",
+    },
+    imageURL: {
+      type: String,
+      default: "none",
+    },
+
+  },
+};
+</script>
+    
+<style scoped>
+</style>
+    
